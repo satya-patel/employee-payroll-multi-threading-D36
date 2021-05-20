cbe9fbb9f61a616d430edbdad52ba4e8c46e1cfe
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-20 13:53:50 +0530

    uc2 and uc3 added

--- lib/src/main/java/com/EmployeePayroll_MultiThreading/EmployeePayrollDBService.java
+++ lib/src/main/java/com/EmployeePayroll_MultiThreading/EmployeePayrollDBService.java
@@ -10,9 +10,12 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class EmployeePayrollDBService {
 	private static EmployeePayrollDBService employeePayrollDBService;
+	private static Logger log = Logger.getLogger(EmployeePayrollDBService.class.getName());
+	private int connectionCounter = 0;
 
 	// creating the object of Signature and getting instance
 	public static EmployeePayrollDBService getInstance() {
@@ -22,17 +25,18 @@ public class EmployeePayrollDBService {
 		return employeePayrollDBService;
 	}
 
-	public static Connection getConnection() {
+	public synchronized Connection getConnection() throws SQLException {
+		connectionCounter++;
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
 		String userName = "root";
 		String password = "13041997@Mda";
 		Connection connection = null;
 		// the DriverManager class will attempt to load available JDBC drivers
-		try {
-			connection = DriverManager.getConnection(jdbcURL, userName, password);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		log.info("Processing Thread : " + Thread.currentThread().getName() + "Connecting to database : " + jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, password);
+		//which thread is actually going to execute my connection
+		log.info("Processing Thread : " + Thread.currentThread().getName() + " ID : " + connectionCounter
+				+ " Connection is successful! " + connection);
 		return connection;
 	}
 
@@ -49,7 +53,7 @@ public class EmployeePayrollDBService {
 				String name = resultSet.getString("name");
 				String gender = resultSet.getString("Gender");
 				double salary = resultSet.getDouble("salary");
-				LocalDate startDate = resultSet.getDate("start").toLocalDate();
+				LocalDate startDate = resultSet.getDate("startDate").toLocalDate();
 				employeePayrollList.add(new EmployeePayrollData(emp_id, name, gender, salary, startDate));
 			}
 		} catch (Exception e) {
